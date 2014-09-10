a536f693b79bbae334edfc1004db4aed0fc90e68
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2014-09-10 17:37:49 +0200

    Port system is better now

---
+++ port/arm/omap2/plat.c
@@ -0,0 +1,24 @@
+/*
+ * plat.c
+ * This file is part of module_template
+ *
+ * Copyright (C) 2014 - Nenad Radulović
+ *
+ * module_template is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * module_template is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with module_template. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+void some(void) 
+{
+
+}
