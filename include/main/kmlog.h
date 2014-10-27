6de3aa6c8a3adb0cb06d9e6f69b01ed5abfdf110
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2014-10-27 12:23:24 +0100

    Folder renaming

---
+++ include/main/kmlog.h
@@ -0,0 +1,41 @@
+/*
+ * log.h
+ * This file is part of module_template
+ *
+ * Copyright (C) 2014 - Nenad Radulović
+ *
+ * module_template is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * module_template is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with module_template. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef KMLOG_H
+#define KMLOG_H
+
+#define KML_OFF                 0
+
+#define KML_ERR                 1
+#define KML_WARN                2
+#define KML_INFO                3
+#define KML_DBG                 4
+
+#define KML_GLOBAL_LEVEL        KML_DBG
+
+#define KMLOG(level, ...)                                                       \
+    do {                                                                        \
+        if (level < KML_GLOBAL_LEVEL) {                                         \
+            printk(# __VA_ARGS__);                                             \
+        }                                                                       \
+    } while (0)
+
+#endif /* KMLOG_H */
+
